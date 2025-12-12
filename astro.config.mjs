fe5ea57958a29c13d99883e14d31361467004bc8
Henok Wehibe <henokmicrometre@gmail.com>
2025-12-12 01:48:44 +0000

    added embadings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,19 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://henok.cloud', // Add your domain here
-  integrations: [react(), mdx()],
+  integrations: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()]
